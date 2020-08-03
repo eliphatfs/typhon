30ae3657794fed6bdecefff3e1e3f5a59e7d7d7a
北海若 <shiruoxi2010@163.com>
2020-08-03 19:57:06 +0800

    Module Inits

---
+++ typhon/core/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 18:47:29 2020
+
+@author: eliphat
+"""
+from . import stack_machine, type_system, codegen
---
+++ typhon/core/type_system/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 12:01:32 2020
+
+@author: eliphat
+"""
+from . import base_types, inference, concepts
