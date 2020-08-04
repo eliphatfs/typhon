a5d7c26c4f21ee3e786e3c30229cbe60163dc5d1
北海若 <shiruoxi2010@163.com>
2020-08-04 11:35:14 +0800

    Changed Generated Code Formatting

--- typhon/__init__.py
+++ typhon/__init__.py
@@ -23,11 +23,11 @@ def generate_c(func):
     type_header = "".join(map(lambda t: t.definition, types))
     impl_header = "\n".join(map(lambda i: core.codegen.generate(i),
                                 result.impls))
-    main_header = "int main() {\n\t"
+    main_header = "int main() {\n    "
     main_variables = []
     for var in result.variables.values():
-        main_variables.append("%s %s;\n\t" % (var.py_type.c_name, var.name))
-    main_body = ';\n\t'.join(result.body)
+        main_variables.append("%s %s;\n    " % (var.py_type.c_name, var.name))
+    main_body = ';\n    '.join(result.body)
     main_tail = ";\n}\n"
     return (file_header + '\n' + type_header + '\n' + impl_header
             + main_header + ''.join(main_variables) + main_body + main_tail)
