07725d2fe54aec6872eb5f5398115ce595833e8c
eliphat.f.s <shiruoxi2010@163.com>
2021-03-13 23:02:56 +0800

    Bugfix in BaseConstraint definition

--- typhon/core/type_system/constraints/base_constraint.py
+++ typhon/core/type_system/constraints/base_constraint.py
@@ -6,14 +6,14 @@ Created on Sat Mar 13 21:31:02 2021
 """
 
 class BaseConstraint:
-    def cause_vars():
+    def cause_vars(self):
         raise NotImplementedError()
 
-    def effect_vars():
+    def effect_vars(self):
         raise NotImplementedError()
 
-    def fix():
+    def fix(self):
         raise NotImplementedError()
 
-    def is_resolved():
+    def is_resolved(self):
         raise NotImplementedError()
