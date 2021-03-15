dd3000c329257f60a765113efd7aa895e91a867a
eliphat.f.s <shiruoxi2010@163.com>
2021-03-15 09:40:45 +0800

    Imports for new constraints

--- typhon/core/type_system/__init__.py
+++ typhon/core/type_system/__init__.py
@@ -9,3 +9,7 @@ from .type_repr import RecordType, FunctionType, BottomType, TyphonType
 from .system import TypeSystem
 from .constraints.base_constraint import BaseConstraint
 from .constraints.fixed_constraint import FixedConstraint
+from .constraints.func_call_constraint import FuncCallConstraint
+from .constraints.member_constraint import MemberConstraint
+from .constraints.subtype_constraint import SubtypeConstraint
+from .constraints.equality_constraint import EqualityConstraint
