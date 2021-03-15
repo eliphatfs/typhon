65c3d78b71bd96ed89b33a4df1ebb3d9b8fb82d2
eliphat.f.s <shiruoxi2010@163.com>
2021-03-15 17:11:18 +0800

    Fixed a import order conflict

--- typhon/core/type_system/__init__.py
+++ typhon/core/type_system/__init__.py
@@ -6,8 +6,8 @@ Created on Sat Mar 13 22:09:19 2021
 """
 from .type_var import TypeVar
 from .type_repr import RecordType, FunctionType, BottomType, TyphonType
-from .system import TypeSystem
 from .constraints.base_constraint import BaseConstraint
+from .system import TypeSystem
 from .constraints.fixed_constraint import FixedConstraint
 from .constraints.func_call_constraint import FuncCallConstraint
 from .constraints.member_constraint import MemberConstraint
