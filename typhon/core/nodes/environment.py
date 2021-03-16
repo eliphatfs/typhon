4c8ed28d8124b8fe681788b1ec8f728de85f80b6
eliphat.f.s <shiruoxi2010@163.com>
2021-03-16 22:25:46 +0800

    Tree structure for NodeEnv

--- typhon/core/nodes/environment.py
+++ typhon/core/nodes/environment.py
@@ -7,11 +7,18 @@ Created on Mon Mar 15 10:46:02 2021
 
 
 class NodeEnv:
-    def __init__(self, qualname):
+    def __init__(self, qualname: str, parent):
         self.qualname = qualname
         self.bindings = dict()
+        self.children = list()
+        if parent is not None:
+            parent.children.append(self)
+        self.parent = parent
 
     def query_name(self, name):
         if name in self.bindings:
             return self.bindings[name]
-        raise NameError("Attempting to load unbound name %s" % name)
+        if self.parent is not None:
+            return self.parent.query_name(name)
+        else:
+            raise NameError("Attempting to load unbound name %s" % name)
