9c66fb0c139b3f82922b0b2f6a2dc31db1319b2e
eliphat.f.s <shiruoxi2010@163.com>
2021-03-23 20:07:24 +0800

    Func Def Node

--- typhon/core/nodes/func_def_node.py
+++ typhon/core/nodes/func_def_node.py
@@ -4,7 +4,13 @@ Created on Mon Mar 22 11:03:44 2021
 
 @author: eliphat
 """
-from .base_node import StmtNode
+from .base_node import StmtNode, RootNodeMixin
 
-class FuncDefNode(StmtNode):
-    pass
+
+class FuncDefNode(StmtNode, RootNodeMixin):
+    def __init__(self, env, body):
+        super().__init__(env)
+        self.body = body
+
+    def children(self):
+        return self.body
