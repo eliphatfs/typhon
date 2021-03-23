aee01bb69485bbb66125b07b81d11263bf41c5c8
eliphat.f.s <shiruoxi2010@163.com>
2021-03-23 20:07:16 +0800

    Return Node

--- typhon/core/nodes/return_node.py
+++ typhon/core/nodes/return_node.py
@@ -5,7 +5,20 @@ Created on Mon Mar 22 10:14:37 2021
 @author: eliphat
 """
 from .base_node import StmtNode
+from ..type_system import SubtypeConstraint
 
 
 class ReturnStmtNode(StmtNode):
-    pass  # TODO: implement
+    def __init__(self, env, expr):
+        super().__init__(env)
+        self.expr = expr
+
+    def children(self):
+        return [self.expr]
+
+    def typing(self, ts):
+        abs_var = self.env.query_name("@RET")
+        ts.add_constraint(SubtypeConstraint(
+            abs_var.TV,
+            self.expr.value_type_var()
+        ))
