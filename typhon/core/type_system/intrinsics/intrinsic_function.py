55d88e84d8b99656f19ec38e5327ac1472cf24b2
eliphat.f.s <shiruoxi2010@163.com>
2021-03-24 16:13:53 +0800

    Intrinsic Function

---
+++ typhon/core/type_system/intrinsics/intrinsic_function.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 25 17:04:38 2021
+
+@author: eliphat
+"""
+from ..type_repr import TyphonType, BottomType
+
+
+class IntrinsicFunction(TyphonType):
+    def __call__(self, out_type_var, *arg_type_vars):
+        pass
+
+
+class ArrowTypeCollectionIntrinsicFunction(list, IntrinsicFunction):
+    def __call__(self, out_type_var, *arg_type_vars):
+        for arrow in self:
+            if len(arrow.args) != len(arg_type_vars):
+                continue
+            for arg_gt, arg_var in zip(arrow.args, arg_type_vars):
+                if arg_gt != arg_var.T:
+                    break
+            else:
+                out_type_var.T = arrow.r
+                return
+        if not any(isinstance(argv.T, BottomType) for argv in arg_type_vars):
+            raise TypeError("Intrinsic type error.")
+            # TODO: meaningful error message
+
+    def __or__(self, o):
+        if isinstance(o, ArrowTypeCollectionIntrinsicFunction):
+            return ArrowTypeCollectionIntrinsicFunction(
+                [a for a in self if a in o]
+            )
+        return NotImplemented
