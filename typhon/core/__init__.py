a70cbda9a726110e16a05d31c8955e05eaff6ca2
eliphat.f.s <shiruoxi2010@163.com>
2021-03-26 16:36:36 +0800

    Core helper function

--- typhon/core/__init__.py
+++ typhon/core/__init__.py
@@ -4,4 +4,23 @@ Created on Mon Mar 15 16:52:48 2021
 
 @author: eliphat
 """
+import ast
+import collections
 from . import nodes, prepass, type_system
+
+
+InferenceResult = collections.namedtuple(
+    "InferenceResult",
+    ["ts", "env", "ast"]
+)
+
+
+def type_infer(code, prog_name="<gamma>"):
+    ts = type_system.TypeSystem(prog_name)
+    env_root = nodes.environment.NodeEnv("", None)
+    py_ast = ast.parse(code)
+    desugar_ast = prepass.desugar.run_desugar(py_ast)
+    typhon_ast = prepass.tree_transformer.typhon_tree(env_root, desugar_ast)
+    typhon_ast.typing_all_subs(ts)
+    ts.solve()
+    return InferenceResult(ts, env_root, typhon_ast)
