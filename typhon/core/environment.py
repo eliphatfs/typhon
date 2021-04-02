5317ad3ad36e368e64b1246523acbe39320b411b
eliphat.f.s <shiruoxi2010@163.com>
2021-04-02 09:37:08 +0800

    Environment: Generator log data

--- typhon/core/environment.py
+++ typhon/core/environment.py
@@ -9,8 +9,9 @@ from .type_system.intrinsics import builtin_functions
 
 
 class NodeEnv:
-    def __init__(self, qualname: str, parent):
+    def __init__(self, qualname: str, parent, generator=None):
         self.qualname = qualname
+        self.generator = generator
         self.bindings = dict()  # Python names
         self.symbols = collections.OrderedDict()  # Let-bound symbol
         self.children = list()
